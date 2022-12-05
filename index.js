e0797f3be33afdb252d8eaf9cba79b9fa7e3c472
bahnisikha <bahnisikhadhar@gmail.com>
2022-12-05 10:45:03 +0530

    added tooltip for video icon and also decorated the dropdown nd hamburger dropdown

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
+const mainBody=document.querySelector("main");
+
+//-----------------------------------------FOR THE STAR'S COLOR CHANGE ON CLICK----------------------------------
+
 const star=document.querySelector(".fa-star");
 star.addEventListener("click",(event)=>{
     star.classList.toggle("stars")
 })
 
+//--------------------------------------------DROP DOWN PART IN FILE SECTION---------------------------------------
 const dropDown=document.querySelector(".drop_down");
 const fileMenu=document.querySelector(".file_menu");
 fileMenu.addEventListener("click",()=>{
     dropDown.classList.toggle("display_block")
 })
+mainBody.addEventListener("click",()=>{
+    dropDown.classList.remove("display_block");
+})
 
 
-
-//---------------------------------------FOR PHONE VIEW----------------------------------------------------
+//---------------------------------------FOR PHONE VIEW--------------------------------------------------------------
 
 const special=document.querySelector(".special_entry");
 const phoneBar=document.querySelector(".phone_bar");
+
+
 phoneBar.addEventListener("click",()=>{
     special.classList.toggle("display_block")
 })
 
-s
+mainBody.addEventListener("click",()=>{
+    special.classList.remove("display_block");
+})
